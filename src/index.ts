23e2b37904fae46ca4184f76e29237511d1189bc
Caalb <carlosmunizfjunior@gmail.com>
2023-07-16 16:56:38 -0300

    chore: first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+class Person {
+  sayHello() {
+    return "Hello World";
+  }
+}
+app.get("/", (req: Request, res: Response) => {
+  res.send(new Person().sayHello());
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+export default Person;
